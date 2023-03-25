cb984e28129d2218a4df032ea9b2a659c75abd25
David Stockdale <dstock3@gmail.com>
2023-03-24 22:09:13 -0500

    created about page placeholder content.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../../style/about.css'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-container">
+      <h1>About AI Watch</h1>
+      <p>
+        AI Watch is a YouTube channel dedicated to providing the latest news, insights, and
+        discussions about artificial intelligence. Our goal is to educate and inspire our
+        viewers with high-quality content that explores the ever-evolving world of AI.
+      </p>
+      <p>
+        If you're interested in learning more about AI or staying up-to-date with the latest
+        developments, subscribe to AI Watch and join our growing community of AI enthusiasts.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
