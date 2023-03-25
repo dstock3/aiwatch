769f73a1aa4750ce78b5cb9f9cb6ff33c6ef357c
dstock3 <dstock3@gmail.com>
2023-03-25 18:26:18 -0500

    Added sidebar to blog page.

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,44 +1,12 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 
 const Blog = () => {
   return (
     <Container className="my-5">
       <Row>
-        <Col xs={12} lg={{ span: 8, offset: 2 }}>
-          <Card className="mb-4">
-            <Card.Img
-              variant="top"
-              src="https://via.placeholder.com/900x300"
-              alt="Sample Blog Post"
-            />
-            <Card.Body>
-              <Card.Title>Sample Blog Post Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                by John Doe - March 1, 2023
-              </Card.Subtitle>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="mb-4">
-            <Card.Img
-              variant="top"
-              src="https://via.placeholder.com/900x300"
-              alt="Sample Blog Post"
-            />
-            <Card.Body>
-              <Card.Title>Sample Blog Post Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                by John Doe - March 1, 2023
-              </Card.Subtitle>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="mb-4">
+        <Col md={8}>
+        <Card className="mb-4">
             <Card.Img
               variant="top"
               src="https://via.placeholder.com/900x300"
@@ -55,9 +23,31 @@ const Blog = () => {
             </Card.Body>
           </Card>
         </Col>
+        <Col md={4}>
+          <h4>Previous Blog Entries</h4>
+          <ListGroup>
+            <ListGroup.Item action href="#blog-entry-1">
+              Blog Entry 1
+            </ListGroup.Item>
+            <ListGroup.Item action href="#blog-entry-2">
+              Blog Entry 2
+            </ListGroup.Item>
+            <ListGroup.Item action href="#blog-entry-3">
+              Blog Entry 3
+            </ListGroup.Item>
+            <ListGroup.Item action href="#blog-entry-4">
+              Blog Entry 4
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
       </Row>
     </Container>
   );
 };
 
 export default Blog;
+
+
+
+
+
