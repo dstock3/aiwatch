d2adda4489fe14376548a3cdd30e0fe3e4b7995d
David Stockdale <dstock3@gmail.com>
2023-03-25 01:16:12 -0500

    started building out the blog page component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import '../../style/about.css'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const About = () => {
   return (
-    <div className="about-container">
-      <h1>About AI Watch</h1>
-      <p>
-        AI Watch is a YouTube channel dedicated to providing the latest news, insights, and
-        discussions about artificial intelligence. Our goal is to educate and inspire our
-        viewers with high-quality content that explores the ever-evolving world of AI.
-      </p>
-      <p>
-        If you're interested in learning more about AI or staying up-to-date with the latest
-        developments, subscribe to AI Watch and join our growing community of AI enthusiasts.
-      </p>
-    </div>
+    <Container className="my-5">
+      <Row>
+        <Col xs={12} md={{ span: 8, offset: 2 }}>
+          <h1 className="mb-4">About AI Watch</h1>
+          <p>
+            AI Watch is a YouTube channel dedicated to providing the latest news, insights, and
+            discussions about artificial intelligence. Our goal is to educate and inspire our
+            viewers with high-quality content that explores the ever-evolving world of AI.
+          </p>
+          <p>
+            Our team consists of experts in the field of artificial intelligence, machine learning,
+            and data science. We strive to bring you engaging and informative content that covers
+            a wide range of topics, from AI research breakthroughs to practical applications in
+            various industries.
+          </p>
+          <p>
+            If you're interested in learning more about AI or staying up-to-date with the latest
+            developments, subscribe to AI Watch and join our growing community of AI enthusiasts.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default About;
+
+
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <Container className="my-5">
+      <Row>
+        <Col xs={12} lg={{ span: 8, offset: 2 }}>
+          <Card className="mb-4">
+            <Card.Img
+              variant="top"
+              src="https://via.placeholder.com/900x300"
+              alt="Sample Blog Post"
+            />
+            <Card.Body>
+              <Card.Title>Sample Blog Post Title</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                by John Doe - March 1, 2023
+              </Card.Subtitle>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mb-4">
+            <Card.Img
+              variant="top"
+              src="https://via.placeholder.com/900x300"
+              alt="Sample Blog Post"
+            />
+            <Card.Body>
+              <Card.Title>Sample Blog Post Title</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                by John Doe - March 1, 2023
+              </Card.Subtitle>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mb-4">
+            <Card.Img
+              variant="top"
+              src="https://via.placeholder.com/900x300"
+              alt="Sample Blog Post"
+            />
+            <Card.Body>
+              <Card.Title>Sample Blog Post Title</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                by John Doe - March 1, 2023
+              </Card.Subtitle>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Blog
+export default Blog;
