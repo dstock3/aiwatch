35545721b2c878e51956b04cf35f7e6dcc789be4
dstock3 <dstock3@gmail.com>
2023-03-26 12:34:35 -0500

    added functionality for user to select blog

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import BlogList from '../sections/BlogList';
 
@@ -6,44 +6,54 @@ const Blog = () => {
   const blogs = [
     { title: 'Blog Entry 1',
       date: 'March 1, 2023',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.',
+      img: 'https://via.placeholder.com/900x300'
     },
     { title: 'Blog Entry 2',
       date: 'March 3, 2023',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.',
+      img: 'https://via.placeholder.com/900x300'
     },
     { title: 'Blog Entry 3',
       date: 'March 5, 2023',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.',
+      img: 'https://via.placeholder.com/900x300'
     },
     { title: 'Blog Entry 4',
       date: 'March 9, 2023',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.',
+      img: 'https://via.placeholder.com/900x300'
     }
   ];
+
+  const [selectedBlog, setSelectedBlog] = useState(blogs[0]);
+
+  const handleBlogSelect = (blog) => {
+    setSelectedBlog(blog);
+  }
 
   return (
     <Container className="my-5">
       <Row>
         <Col md={8}>
-        <Card className="mb-4">
-            <Card.Img
-              variant="top"
-              src="https://via.placeholder.com/900x300"
-              alt="Sample Blog Post"
-            />
-            <Card.Body>
-              <Card.Title>Sample Blog Post Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                by John Doe - March 1, 2023
-              </Card.Subtitle>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          {selectedBlog && (
+            <Card className="mb-4">
+              <Card.Img
+                variant="top"
+                src={selectedBlog.img}
+                alt={selectedBlog.title}
+              />
+              <Card.Body>
+                <Card.Title>{selectedBlog.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {selectedBlog.date}
+                </Card.Subtitle>
+                <Card.Text>{selectedBlog.text}</Card.Text>
+              </Card.Body>
+            </Card>
+          )}
         </Col>
-        <BlogList blogs={blogs} />
+        <BlogList blogs={blogs} onBlogSelect={handleBlogSelect} />
       </Row>
     </Container>
   );
--- src/components/sections/BlogList.js
+++ src/components/sections/BlogList.js
@@ -1,14 +1,38 @@
+import React from 'react';
 import { Col, ListGroup } from 'react-bootstrap';
+import '../../style/blog.css'
 
 const BlogList = (props) => {
+    const [activeIndex, setActiveIndex] = React.useState(null);
+
+    const handleMouseEnter = (index) => {
+        setActiveIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setActiveIndex(null);
+    };
 
     return (
-        <Col md={4}>
-            <h4>Previous Blog Entries</h4>
+        <Col md={3}>
+            <h4 className="text-center mb-4">Previous Blog Entries</h4>
             <ListGroup>
                 {props.blogs.map((blog, index) => (
-                    <ListGroup.Item key={index} action href={`#blog-entry-${index + 1}`}>
-                        {blog.title}
+                    <ListGroup.Item
+                        key={index}
+                        onClick={() => props.onBlogSelect(blog)}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave()}
+                        active={activeIndex === index}
+                    >
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="title">
+                                {blog.title}
+                            </div>
+                            <div className="date">
+                                {blog.date}
+                            </div>
+                        </div>
                     </ListGroup.Item>
                 ))}
             </ListGroup>
