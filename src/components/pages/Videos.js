baeaacd10c7b060edead90a57c759a7c61a8bf1e
dstock3 <dstock3@gmail.com>
2023-03-28 22:23:47 -0500

    refactored Video component to loop through a list of videos.

--- src/components/pages/Videos.js
+++ src/components/pages/Videos.js
@@ -4,6 +4,27 @@ import '../../style/videos.css';
 import { Helmet } from 'react-helmet';
 
 const Videos = () => {
+  const videos = [
+    {
+      title: 'Sample Video Title 1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.',
+      embedUrl: 'https://www.youtube.com/embed/rrstrOrJxOc',
+    },
+    {
+      title: 'Sample Video Title 2',
+      description:
+        'Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet.',
+      embedUrl: 'https://www.youtube.com/embed/rrstrOrJxOc',
+    },
+    {
+      title: 'Sample Video Title 3',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.',
+      embedUrl: 'https://www.youtube.com/embed/rrstrOrJxOc',
+    },
+  ];
+
   return (
     <>
       <Helmet>
@@ -12,54 +33,22 @@ const Videos = () => {
       <Container className="my-5">
         <Row>
           <Col xs={12}>
-            <Card className="mb-4 video-card">
-              <div className="embed-responsive embed-responsive-16by9 video-container">
-                <iframe
-                  className="embed-responsive-item"
-                  title="Sample Video"
-                  src="https://www.youtube.com/embed/rrstrOrJxOc"
-                  allowFullScreen
-                ></iframe>
-              </div>
-              <Card.Body>
-                <Card.Title>Sample Video Title</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card className="mb-4 video-card">
-              <div className="embed-responsive embed-responsive-16by9 video-container">
-                <iframe
-                  className="embed-responsive-item"
-                  title="Sample Video"
-                  src="https://www.youtube.com/embed/rrstrOrJxOc"
-                  allowFullScreen
-                ></iframe>
-              </div>
-              <Card.Body>
-                <Card.Title>Sample Video Title</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card className="mb-4 video-card">
-              <div className="embed-responsive embed-responsive-16by9 video-container">
-                <iframe
-                  className="embed-responsive-item"
-                  title="Sample Video"
-                  src="https://www.youtube.com/embed/rrstrOrJxOc"
-                  allowFullScreen
-                ></iframe>
-              </div>
-              <Card.Body>
-                <Card.Title>Sample Video Title</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Duis elementum euismod quam, vitae condimentum mi ullamcorper vel.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            {videos.map((video, index) => (
+              <Card key={index} className="mb-4 video-card">
+                <div className="embed-responsive embed-responsive-16by9 video-container">
+                  <iframe
+                    className="embed-responsive-item"
+                    title={video.title}
+                    src={video.embedUrl}
+                    allowFullScreen
+                  ></iframe>
+                </div>
+                <Card.Body>
+                  <Card.Title>{video.title}</Card.Title>
+                  <Card.Text>{video.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
           </Col>
         </Row>
       </Container>
