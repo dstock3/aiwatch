e5239392ef2aa29e0d5f025e1b8a9fb03c0719bc
dstock3 <dstock3@gmail.com>
2023-04-01 11:28:36 -0500

    Added papers to resources page.

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -6,20 +6,30 @@ import '../../style/resources.css';
 const Resources = () => {
   const resources = [
     {
-      title: 'Resource 1',
-      description: 'This is a description of Resource 1.',
-      url: 'https://example.com/resource1'
+      title: 'HuggingGPT: Solving AI Tasks with ChatGPT and its Friends in HuggingFace',
+      description: 'The paper introduces HuggingGPT, a system that leverages large language models to connect and manage various AI models across domains and modalities, enabling the handling of complicated tasks and paving a new way towards artificial general intelligence.',
+      url: 'https://arxiv.org/pdf/2303.17580.pdf'
     },
     {
-      title: 'Resource 2',
-      description: 'This is a description of Resource 2.',
-      url: 'https://example.com/resource2'
+      title: 'GPTs are GPTs: An Early Look at the Labor Market Impact Potential of Large Language Models',
+      description: 'This study investigates the potential impact of large language models on the U.S. labor market, finding that a significant portion of the workforce could have their tasks affected by LLMs and LLM-powered software, with substantial economic, social, and policy implications.',
+      url: 'https://arxiv.org/pdf/2303.10130.pdf'
     },
     {
-      title: 'Resource 3',
-      description: 'This is a description of Resource 3.',
-      url: 'https://example.com/resource3'
-    }
+      title: 'Explicit Planning Helps Language Models in Logical Reasoning',
+      description: 'This paper presents a novel system that incorporates explicit planning for multi-step logical reasoning in language models, resulting in significant performance improvements over competing systems.',
+      url: 'https://arxiv.org/pdf/2303.15714.pdf'
+    },
+    {
+      title: 'Can AI-Generated Text be Reliably Detected?',
+      description: 'This paper discusses the unreliability of AI-generated text detectors in practical scenarios and the potential consequences, emphasizing the need for an honest conversation about the ethical and responsible use of LLMs.',
+      url: 'https://arxiv.org/pdf/2303.11156.pdf'
+    },
+    {
+      title: 'Scaling Expert Language Models with Unsupervised Domain Discovery',
+      description: 'This paper presents an asynchronous method for training large, sparse language models by clustering related documents, reducing communication overhead and improving performance compared to dense baselines.',
+      url: 'https://arxiv.org/pdf/2303.14177.pdf'
+    },
   ];
 
   return (
